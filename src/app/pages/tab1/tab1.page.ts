fc089974b9947a96c103ff7fe12b801347dc6b0e
Konaka <konaka@KonakanoiMac.local>
2019-03-30 11:53:00 +0900

    検索機能

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -8,15 +8,39 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page {
   personalist: Array<any>;
+  searchMode = false;
+  searchInput = '';
+  showSearchCancelButton = true;
+  personalistAll: Array<any>;
+
   constructor(private router: Router) {
     const persona = new PersonaDb();
     // this.personalist = persona.persona.sort((one, two) => one.name > two.name ? 1 : -1);
-    this.personalist = persona.persona.sort((one, two) => one.level > two.level ? 1 : -1);
+    this.personalistAll = persona.persona.sort((one, two) => one.level > two.level ? 1 : -1);
+    this.personalist = JSON.parse(JSON.stringify(this.personalistAll));
+
   }
 
   onItemClick(persona) {
     console.log("click " + persona.name);
-    
+
     this.router.navigateByUrl('/persona-info/' + persona.name);
+  }
+
+  onInput(input) {
+    console.log(input);
+    
+    if (input.detail === null || input.detail.target.value === '') {
+      this.personalist = this.personalistAll;
+      return;
+    }
+    console.log(input.detail.target.value);
+    const searchInput = input.detail.target.value;
+    this.personalist = this.personalistAll.filter(persona => persona.name.indexOf(searchInput) > -1);
+
+  }
+  onCancel(input) {
+    console.log('cancel');
+
   }
 }
